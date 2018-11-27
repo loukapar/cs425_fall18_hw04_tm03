447426f528fa12e7e0891d49927f39b4480f8924
vas <loukapar@gmail.com>
2018-11-27 23:24:32 +0200

    update, create, store Image first attemt

--- api/storeImage.php
+++ api/storeImage.php
@@ -12,6 +12,8 @@
 	$target_dir = "cs425_fall18_hw04_tm03/resources/";
 	$target_file = $target_dir . "pv_" . $_POST["pvID"]
 	$uploadOk = 1;
+	echo "hi";
+	/*
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 	// Check if image file is a actual image or fake image
 	if(isset($_POST["uploadImage"])) {
@@ -24,8 +26,7 @@
 			$uploadOk = 0;
 		}
 	}
-echo "hi";
-	/*
+
 	//if (file_exists($target_file)) {
 	//	echo "Sorry, file already exists.";
 	//	$uploadOk = 0;
