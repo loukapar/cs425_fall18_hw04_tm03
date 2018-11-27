bf10a37803e483faa6a52eae7009e7a13e940bf8
vas <loukapar@gmail.com>
2018-11-27 19:23:28 +0200

    fix database

--- config/Database.php
+++ config/Database.php
@@ -12,7 +12,7 @@
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';port=3306;dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
