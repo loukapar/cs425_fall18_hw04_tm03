b5f8a38666acbcc3583b71127695b53b8baaa7f1
vas <loukapar@gmail.com>
2018-11-28 20:47:07 +0200

    upload image

--- api/uploadImage.php
+++ api/uploadImage.php
@@ -1,6 +1,5 @@
 <?php
 $target_dir = "../resources/";
-$_FILES["fileToUpload"]["name"] = "pv1";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -36,8 +35,9 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
+	$target_file = $target_dir . "pv1" . $imageFileType;
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". $target_file . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
