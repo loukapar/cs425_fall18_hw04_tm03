0e3b230678c504ff923e16b62510f581f09722ab
vas <loukapar@gmail.com>
2018-11-30 23:55:55 +0200

    update with image

--- models/Pv.php
+++ models/Pv.php
@@ -189,7 +189,7 @@ class Pv {
 		return $array;
 		
 	}
-
+/*
 	public function update($data) {
 		//create query
 		$query = 'UPDATE ' . $this->table . ' SET ';
@@ -226,7 +226,7 @@ class Pv {
 
 		return false;
     }
-	/*
+	
 		
 	    // Delete pv
     public function delete($pv_id) {
