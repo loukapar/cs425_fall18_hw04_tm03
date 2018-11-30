1dbc4d5b5ad8478d763c9a162418cb5c7e7bf5c3
vas <loukapar@gmail.com>
2018-12-01 00:24:42 +0200

    update with image

--- api/update.php
+++ api/update.php
@@ -6,6 +6,8 @@
 	header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
 	include_once '../config/Database.php';
+	include_once '../models/Pv.php';
+	
 	// Instantiate DB & connect
 	$database = new Database();
 	$db = $database->connect();
