a7c80328576d3863184ab6db3ad3e0ff4593dbf8
vas <loukapar@gmail.com>
2018-12-01 17:14:09 +0200

    delete pv

--- api/delete.php
+++ api/delete.php
@@ -16,7 +16,9 @@
 	// Get raw posted data
 	$data = json_decode(file_get_contents("php://input"));
 	$pv->pv_id = $data->pv_id;
+	echo json_encode($pv->pv_id);
 	// Delete post
+	/*
 	if($pv->delete()) {
 		echo json_encode(
 		array('message' => 'PV deleted')
@@ -26,4 +28,5 @@
 		array('message' => 'PV deleted')
 		);
 	}
+	*/
 ?>
