4fedf6b23fb19ebac514a9744aa06b0193e26c62
vas <loukapar@gmail.com>
2018-12-01 20:23:18 +0200

    auth

--- api/authentication.php
+++ api/authentication.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if(empty($_SESSION["authenticated"]) || $_SESSION["authenticated"] != 'true') {
-    header('Location: ../front_end/index.html');
+    header('Location: 52.26.216.32/cs425_fall18_hw04_tm03/front_end/index.html');
 }
 ?>
