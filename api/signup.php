97584998fa46852a32def300fad06fcaec79399e
vas <loukapar@gmail.com>
2018-12-02 19:22:15 +0200

    signup

---
+++ api/signup.php
@@ -0,0 +1,36 @@
+<?php 
+	// Headers
+	header('Access-Control-Allow-Origin: *');
+	header('Content-Type: application/json');
+	header('Access-Control-Allow-Methods: POST');
+	header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+	
+	include_once '../config/Database.php';
+	
+	// Instantiate DB & connect
+	$database = new Database();	
+	$db = $database->connect();
+	
+	// Get raw posted data
+	$data = json_decode(file_get_contents("php://input"));
+	
+	$username = htmlspecialchars(strip_tags($data->username)); 
+	$password = htmlspecialchars(strip_tags($data->password)); 
+	
+	$query = 'INSERT INTO USER SET username = :username, password = :password;';
+	$stmt = $db->prepare($query);
+	
+	$stmt->bindParam(':username', $username);
+	$stmt->bindParam(':password', $password);
+	
+	if($stmt->execute()) {
+		echo json_encode(
+			array('message' => 'Account created successfully!', 'stat' => true)
+		);
+	} else {
+		echo json_encode(
+			array('message' => 'Something went wrong. Try again!', 'stat' => false)
+		);
+		
+	}
+?>
