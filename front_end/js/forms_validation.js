9fed788f30a63799d3395b8273305c50b9d2c8c5
fkiolaris <nicolas-95@live.com>
2018-12-02 23:22:46 +0200

    FINAL

--- front_end/js/forms_validation.js
+++ front_end/js/forms_validation.js
@@ -4,13 +4,13 @@ function validateUsername(fld) {
 
     if (fld.value == "") {
         fld.style.background = 'Yellow';
-        error = "You didn't enter a username.\n";
-    } else if ((fld.value.length < 5) || (fld.value.length > 15)) {
+        error = "You didn't enter a name.\n";
+    } else if ((fld.value.length < 5) || (fld.value.length > 30)) {
         fld.style.background = 'Yellow';
-        error = "The username is the wrong length.\n";
+        error = "The name is the wrong length. (max=30)\n";
     } else if (illegalChars.test(fld.value)) {
         fld.style.background = 'Yellow';
-        error = "The usernasme contains illegal characters.\n";
+        error = "The name contains illegal characters.\n";
     } else {
         fld.style.background = 'White';
     }
