75a93a1c49fbe1a5d407d7327d9995d2b828ca52
vas <loukapar@gmail.com>
2018-12-02 23:30:28 +0200

    5 minute block auth

--- api/login.php
+++ api/login.php
@@ -30,7 +30,7 @@
 
 	
 	if (isset($_SESSION['last_login_time'])){
-		if (time() - $_SESSION['last_login_time'] > 10) {
+		if (time() - $_SESSION['last_login_time'] > 5*60) {
 			$_SESSION['times'] = 0;
 			unset($_SESSION['last_login_time']);
 		}
