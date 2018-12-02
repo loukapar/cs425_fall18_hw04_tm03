2192bfa40eab0737505176ad7d1928e2e87836a9
vas <loukapar@gmail.com>
2018-12-02 19:13:06 +0200

    auth

--- api/login.php
+++ api/login.php
@@ -62,7 +62,7 @@
 			$_SESSION["authenticated"] = 'false';
 			if (!isset($_SESSION['last_login_time']))
 				$_SESSION['last_login_time'] = time();
-			$msg = 'You have exceeded the maximum number of attempts. Come back in ' . (($_SESSION['last_login_time'] + 5*60) - time())/60 . 'minutes!';
+			$msg = 'You have exceeded the maximum number of attempts.Try again in 5minutes';
 			echo json_encode(
 				array('message' => $msg, 'stat' => false)
 			);
